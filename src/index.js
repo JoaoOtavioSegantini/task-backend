3e9aa34b0f58de6087e098314cefaaf763ce1e6e
JoaoOtavioSegantini <joaotavioos@outlook.com>
2021-04-11 15:15:05 -0300

    first commit

---
+++ src/index.js
@@ -0,0 +1,13 @@
+const express = require('express')
+const cors = require('cors')
+
+const server = express()
+server.use(cors())
+server.use(express.json())
+
+const TaskRouter = require('./routes/TaskRoutes')
+server.use('/task', TaskRouter)
+
+server.listen(3333, () => {
+    console.log('API Online')
+})
